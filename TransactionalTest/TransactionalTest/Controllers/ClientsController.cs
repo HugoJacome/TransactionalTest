0d86711072fe66cdb93e07fe94c43111fb09d728
HugoJacome <huxajavijanp@hotmail.com>
2022-08-26 00:19:43 -0500

    Controllers
    Add controllers

---
+++ TransactionalTest/TransactionalTest/Controllers/ClientsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransactionalTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() => throw new NotImplementedException();
+        [HttpPost]
+        public IActionResult Post() => throw new NotImplementedException();
+        [HttpPut]
+        public IActionResult Put() => throw new NotImplementedException();
+        [HttpPatch]
+        public IActionResult Push() => throw new NotImplementedException();
+        [HttpDelete]
+        public IActionResult Delete() => throw new NotImplementedException();
+    }
+}
