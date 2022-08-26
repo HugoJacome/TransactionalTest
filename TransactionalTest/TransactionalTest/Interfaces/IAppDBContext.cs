4acb7bb31ffeca47acbc60212b7c5915be0f0e79
HugoJacome <huxajavijanp@hotmail.com>
2022-08-26 00:08:29 -0500

    DBContext EF
    Add dbcontext EF with nugget packages and class with interface for ID

---
+++ TransactionalTest/TransactionalTest/Interfaces/IAppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransactionalTest.Models;
+
+namespace TransactionalTest.Interfaces
+{
+    public interface IAppDBContext
+    {
+        DbSet<Account> Account { get; set; }
+        DbSet<Client> Client { get; set; }
+        DbSet<Movements> Movements { get; set; }
+        DbSet<Person> Person { get; set; }
+    }
+}
