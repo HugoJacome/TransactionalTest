81f31e881fab5495c06d50f1734bc4d8acb8b2e7
HugoJacome <huxajavijanp@hotmail.com>
2022-08-26 00:08:29 -0500

    DBContext EF
    Add dbcontext EF with nugget packages and class with interface for ID

--- TransactionalTest/TransactionalTest/Program.cs
+++ TransactionalTest/TransactionalTest/Program.cs
@@ -1,3 +1,7 @@
+
+using TransactionalTest.Context;
+using TransactionalTest.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// ID
+builder.Services.AddTransient<IAppDBContext, AppDBContext>();
+
+
 
 var app = builder.Build();
 
