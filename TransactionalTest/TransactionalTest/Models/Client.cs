91dd781c043e8ea32c6da5eb42c87e8d080780de
HugoJacome <huxajavijanp@hotmail.com>
2022-08-26 00:07:37 -0500

    Models
    Add models in project

---
+++ TransactionalTest/TransactionalTest/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransactionalTest.Models
+{
+    public class Client: Person
+    {
+        //[Key]
+        //public Guid ClientId { get; set; }
+        public string Password { get; set; }
+        public StateEnum State { get; set; }
+        public Account account { get; set; }
+    }
+}
---
+++ TransactionalTest/TransactionalTest/Models/Movements.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransactionalTest.Models
+{
+    public class Movements
+    {
+        [Key]
+        public Guid MovementId { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime MovementDate { get; set; }
+        public string MovementType { get; set; }
+        public string Value { get; set; }
+        public string Balance { get; set; }
+        public Account account { get; set; }
+    }
+}
