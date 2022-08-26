a6acc6564727f7e8eea7c480af0407cb40c868a0
HugoJacome <huxajavijanp@hotmail.com>
2022-08-26 00:07:37 -0500

    Models
    Add models in project

---
+++ TransactionalTest/TransactionalTest/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransactionalTest.Models
+{
+    public class Account
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[0-9]*$")]
+        public string AccountNumber { get; set; }
+        public AccountTypeEnum AccountType { get; set; }
+        public double OpeningBalance { get; set; }
+        public StateEnum State { get; set; }
+    }
+}
---
+++ TransactionalTest/TransactionalTest/Models/EnumClass.cs
@@ -0,0 +1,21 @@
+﻿namespace TransactionalTest.Models
+{
+    // Enum of StateType
+    public enum StateEnum
+    {
+        Active = 0,
+        Inactive = 1
+    }
+    // Enum for gender
+    public enum GenderEnum
+    {
+        Male = 0,
+        Female = 1
+    }
+    // Enum for gender
+    public enum AccountTypeEnum
+    {
+        Ahorros = 0,
+        Corriente = 1
+    }
+}
