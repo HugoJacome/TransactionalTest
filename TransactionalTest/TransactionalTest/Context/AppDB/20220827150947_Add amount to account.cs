d3e97f472a8a590542962cfd8f0f164a6f4c355a
HugoJacome <huxajavijanp@hotmail.com>
2022-08-27 11:13:27 -0500

    DB context
    Update DB data type to double on value

---
+++ TransactionalTest/TransactionalTest/Context/AppDB/20220827150947_Add amount to account.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransactionalTest.Context.AppDB
+{
+    public partial class Addamounttoaccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Balance",
+                table: "Account",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Account");
+        }
+    }
+}
