0829d03bed5504e84e2258f8be70dc9e5740e46a
HugoJacome <huxajavijanp@hotmail.com>
2022-08-27 11:17:55 -0500

    Services
    Add Compare service and validations

---
+++ TransactionalTest/TransactionalTest/Services/ValidateServices.cs
@@ -0,0 +1,37 @@
+﻿using TransactionalTest.Models;
+using TransactionalTest.Models.Constans;
+using TransactionalTest.Repositories;
+
+namespace TransactionalTest.Services
+{
+    public interface IValidateServices
+    {
+        Task<bool> ValidateMovementQuotaAsync(MovementRequest movement);
+        Task<bool> ValidateAccountBalanceAsync(MovementRequest movement, double balance);
+    }
+    public class ValidateServices : IValidateServices
+    {
+        private IMovementRepository _movementRepository;
+        private IAccountRepository _accountRepository;
+        public ValidateServices(IMovementRepository movementRepository, IAccountRepository accountRepository)
+        {
+            _movementRepository = movementRepository;
+            _accountRepository = accountRepository;
+        }
+
+        public async Task<bool> ValidateAccountBalanceAsync(MovementRequest movement, double balance)
+        {
+            if ((balance + movement.Value) < 0) return false;
+            return true;
+        }
+
+        public async Task<bool> ValidateMovementQuotaAsync(MovementRequest movement)
+        {
+            var movements = await _movementRepository.GetMovementsByDateAsync(movement.MovementDate);
+            if (movements == null) return false;
+            double dailyMovement = movements.Where(m => m.Value < 0).Sum(m => m.Value);
+            if ((dailyMovement + movement.Value) > GlobalConfig.DAILY_AMOUNT_LIMIT) return false;
+            return true;
+        }
+    }
+}
