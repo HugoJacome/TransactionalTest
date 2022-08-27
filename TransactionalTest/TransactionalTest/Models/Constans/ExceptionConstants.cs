d492065187a0ee2d65202854cdbffe9dfa620de2
HugoJacome <huxajavijanp@hotmail.com>
2022-08-27 11:13:58 -0500

    Constants
    Add config and error message constat

---
+++ TransactionalTest/TransactionalTest/Models/Constans/ExceptionConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace TransactionalTest.Models.Constans
+{
+    public class ExceptionConstants
+    {
+
+        public const string UNAVAILABLE_BALANCE = "Saldo no disponible";
+        public const string DAILY_QUOTA_EXCEEDED = "Cupo diario excedido";
+    }
+}
