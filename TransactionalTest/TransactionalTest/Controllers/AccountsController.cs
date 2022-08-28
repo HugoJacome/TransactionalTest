0750aba143c93b327eca539fb4f322523f2ac8d3
HugoJacome <huxajavijanp@hotmail.com>
2022-08-27 19:06:59 -0500

    Add controllers

--- TransactionalTest/TransactionalTest/Controllers/AccountsController.cs
+++ TransactionalTest/TransactionalTest/Controllers/AccountsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using TransactionalTest.Models;
+using TransactionalTest.Models.Constans;
 using TransactionalTest.Repositories;
 using TransactionalTest.Services;
 
@@ -11,12 +12,16 @@ namespace TransactionalTest.Controllers
     public class AccountsController : Controller
     {
         private IAccountRepository _accountRepository;
+        private IClientRepository _clientRepository;
         private ICompareServices _compareServices;
 
-        public AccountsController(IAccountRepository accountRepository, ICompareServices compareServices)
+        public AccountsController(IAccountRepository accountRepository,
+            ICompareServices compareServices,
+            IClientRepository clientRepository)
         {
             _accountRepository = accountRepository;
             _compareServices = compareServices;
+            _clientRepository = clientRepository;
         }
         [HttpGet]
         public async Task<IActionResult> GetAllAccountsAsync()
@@ -33,21 +38,35 @@ namespace TransactionalTest.Controllers
             return Ok(res);
         }
         [HttpPost]
-        public async Task<IActionResult> Post(Account account)
+        public async Task<IActionResult> Post(AccountRequest account)
         {
-            var res = await _accountRepository.CreateAccountAsync(account);
+            var client = await _clientRepository.GetClientByIdentificationAsync(account.ClientIdentification);
+            if (client == null) return NotFound("Cliente no encontrado");
+            Account acc = new Account()
+            {
+                Id = new Guid(),
+                AccountNumber = account.AccountNumber,
+                clientId = client.Id,
+                ClientIdentification = account.ClientIdentification,
+                AccountType = account.AccountType,
+                Balance = account.Balance > 0 ? account.Balance : account.OpeningBalance,
+                OpeningBalance = account.OpeningBalance,
+                State = account.State
+            };
+            var res = await _accountRepository.CreateAccountAsync(acc);
             if (res == null) return NotFound();
             return Ok(res);
         }
         [HttpPut("{Id}")]
         public async Task<IActionResult> PutAsync(Account account, Guid Id)
         {
-            if (Id != account.Id) BadRequest("Error en relacion");
+            if (Id != account.Id) return BadRequest("Error en relacion");
             var accountDB = await _accountRepository.GetAccountByIdAsync(Id);
             if (accountDB == null) return NotFound();
             // update
-            if (_compareServices.CompareAccount(account, accountDB)) BadRequest("cuenta sin cambios");
-            _accountRepository.UpdateAccountById(account);
+            if (_compareServices.CompareAccount(account, accountDB)) return BadRequest("cuenta sin cambios");
+            var result = await _accountRepository.UpdateAccountById(account);
+            if (!result) return BadRequest(ExceptionConstants.ERROR_UPDATE);
             return Ok(account);
         }
         [HttpDelete("{Id}")]
@@ -56,7 +75,8 @@ namespace TransactionalTest.Controllers
             var account = await _accountRepository.GetAccountByIdAsync(Id);
             if (account == null) return NotFound();
             // delete
-            _accountRepository.DeleteAccountById(account);
+            var result = await _accountRepository.DeleteAccountById(account);
+            if (!result) return BadRequest(ExceptionConstants.ERROR_DELETE);
             return Ok(Id);
         }
 
--- TransactionalTest/TransactionalTest/Controllers/ClientsController.cs
+++ TransactionalTest/TransactionalTest/Controllers/ClientsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using TransactionalTest.Models;
+using TransactionalTest.Models.Constans;
 using TransactionalTest.Repositories;
 using TransactionalTest.Services;
 
@@ -12,13 +13,11 @@ namespace TransactionalTest.Controllers
     {
         private IClientRepository _clientRepository;
         private ICompareServices _compareServices;
-        private ILogger _logger;
 
-        public ClientsController(IClientRepository clientRepository, ICompareServices compareServices, ILogger logger)
+        public ClientsController(IClientRepository clientRepository, ICompareServices compareServices)
         {
             _clientRepository = clientRepository;
             _compareServices = compareServices;
-            _logger = logger;
         }
         [HttpGet]
         public async Task<IActionResult> GetAllClientsAsync()
@@ -35,21 +34,34 @@ namespace TransactionalTest.Controllers
             return Ok(res);
         }
         [HttpPost]
-        public async Task<IActionResult> Post(Client client)
+        public async Task<IActionResult> Post(ClientRequest client)
         {
-            var res = await _clientRepository.CreateClientAsync(client);
+
+            var res = await _clientRepository.CreateClientAsync(new Client()
+            {
+                Address = client.Address,
+                Age = client.Age,
+                Gender = client.Gender,
+                Id = new Guid(),
+                Identification = client.Identification,
+                Name = client.Name,
+                Password = client.Password,
+                Phone = client.Phone,
+                State = client.State,
+            });
             if (res == null) return NotFound();
             return Ok(res);
         }
         [HttpPut("{Id}")]
         public async Task<IActionResult> PutAsync(Client client, Guid Id)
         {
-            if (Id != client.Id) BadRequest("Error en relacion");
+            if (Id != client.Id) return BadRequest("Error en relacion");
             var clientDB = await _clientRepository.GetClientByIdAsync(Id);
             if (clientDB == null) return NotFound();
             // update
-            if (_compareServices.CompareClient(client, clientDB)) BadRequest("Cliente sin cambios");
-            _clientRepository.UpdateClientById(client);
+            if (_compareServices.CompareClient(client, clientDB)) return BadRequest("Cliente sin cambios");
+            bool result = await _clientRepository.UpdateClientById(client);
+            if (!result) return BadRequest(ExceptionConstants.ERROR_UPDATE);
             return Ok(client);
         }
         [HttpDelete("{Id}")]
@@ -58,7 +70,8 @@ namespace TransactionalTest.Controllers
             var client = await _clientRepository.GetClientByIdAsync(Id);
             if (client == null) return NotFound();
             // delete
-            _clientRepository.DeleteClientById(client);
+            bool result = await _clientRepository.DeleteClientById(client);
+            if (!result) return BadRequest(ExceptionConstants.ERROR_DELETE);
             return Ok(Id);
         }
 
